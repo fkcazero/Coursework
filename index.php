9fbdbd5190caccd254ad8fb93e17b58a9c014f5a
sergiyzero <sergobala@mail.ru>
2016-12-18 15:00:31 +0200

    add foto from BD

--- index.php
+++ index.php
@@ -15,13 +15,14 @@ $query = mysqli_query( $connection, "SELECT * FROM `auto` ORDER BY time DESC");
 
 while ($row = mysqli_fetch_assoc($query))
 
-{?>
+{
+    ?>
 
     <h1><?php echo $row['title']?></h1>
     <p><?php echo $row['text']?></p>
     <p>Дата публикации: <?php echo $row['date']?> / <?php echo $row['time']?></p>
     <p>Автор новости: <?php echo $row['author']?></p>
-
+    <img src="img/<?php echo $row['file'] ?>" alt="">
     <hr />
 
-<?php }?>
+<?php }
