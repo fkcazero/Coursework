a9535e8f557674f63470c1bf6b39033ef3a42ebf
Lulca <Anatolkruv@gmail.com>
2016-12-18 17:56:23 +0200

    fix spesial img height

--- view/box_special.php
+++ view/box_special.php
@@ -9,5 +9,5 @@
             <p>Blandit varius ut praesent nascetur eu penatibus nisi risus faucibus nunc ornare<br />
                 adipiscing nunc adipiscing. Condimentum turpis massa.</p>
         </header>
-        <span class="image featured"><img src="assets/images/pic01.jpg" alt="" /></span>
+        <span class="image featured"><img src="assets/images/pic01.jpg" alt="" id="height100"/></span>
     </section>
--- view/row.php
+++ view/row.php
@@ -8,7 +8,7 @@
         <div class="6u 12u(narrower)">
 
             <section class="box special">
-                <span class="image featured"><img src="img/<?php echo $row['file']?>" alt="" /></span>
+                <span class="image featured"><img src="img/<?php echo $row['file']?>" alt="" class="height335"/></span>
                 <h3><?php echo $row['title']?></h3>
                 <p>Дата публикации: <?php echo $row['date']?> / <?php echo $row['time']?></p>
                 <p>Автор новости: <?php echo $row['author']?></p>
